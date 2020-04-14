0ec7dce12f9b04550a15cab7a8fdfbbcc64d7a10
zhangch <2732298453@qq.com>
2020-04-14 15:59:46 +0800

    张财华 4.14主支

--- lol-web/src/main/resources/static/js/jquery.pagination.js
+++ lol-web/src/main/resources/static/js/jquery.pagination.js
@@ -1,14 +1,4 @@
-﻿﻿/**
- * This jQuery plugin displays pagination links inside the selected elements.
- * 
- * This plugin needs at least jQuery 1.4.2
- *
- * @author Gabriel Birke (birke *at* d-scribe *dot* de)
- * @version 2.2
- * @param {int} maxentries Number of entries to paginate
- * @param {Object} opts Several options (see README for documentation)
- * @return {Object} jQuery Object
- */
+﻿
  (function($){
 	/**
 	 * @class Class for calculating pagination values
